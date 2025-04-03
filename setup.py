85baae4ec68c2629e768de47203f5a4232d84cde
Matthieu Croissant <matthieu.croissant@gmail.com>
2025-04-03 18:53:01 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='oas_patch',
+    version='1.0.0',
+    description='A tool to apply overlays to OpenAPI documents.',
+    author='Matthieu Croissant',
+    author_email='your.email@example.com',
+    url='https://github.com/mcroissant/oas_patcher',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'PyYAML>=6.0',
+        'jsonpath-ng.ext>=1.5.3'
+    ],
+    entry_points={
+        'console_scripts': [
+            'oas-patch=oas_patcher_cli:cli',  # Update this to reference the correct module
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
---
+++ src/oas_patcher_cli.py
@@ -0,0 +1,56 @@
+"""Module providing the general cli parameters of the oas patcher"""
+
+import argparse
+import json
+import sys
+import yaml
+from file_utils import load_file, save_file
+from overlay import apply_overlay
+
+
+def parse_arguments():
+    """Parse command-line arguments."""
+    parser = argparse.ArgumentParser(
+        description='Apply an OpenAPI Overlay to your OpenAPI document.',
+        epilog='Example usage:\n'
+               'oas-patcher --openapi openapi.yaml --overlay overlay.yaml\
+                   --output modified_openapi.yaml --sanitize',
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+
+    required_group = parser.add_argument_group('Required arguments')
+    required_group.add_argument('--openapi', required=True, help='Path to the OpenAPI description (YAML/JSON).')
+    required_group.add_argument('--overlay', required=True, help='Path to the Overlay document (YAML/JSON).')
+
+    optional_group = parser.add_argument_group('Optional arguments')
+    optional_group.add_argument('--output', required=False, help='Path to save the modified OpenAPI document. Defaults to stdout.')
+    optional_group.add_argument('--sanitize', action='store_true', help='Remove special characters from the OpenAPI document.')
+    return parser.parse_args()
+
+
+def cli():
+    """Command-line interface entry point."""
+    args = parse_arguments()
+
+    # Load input files
+    try:
+        openapi_doc = load_file(args.openapi, args.sanitize)
+        overlay = load_file(args.overlay)
+    except (FileNotFoundError, ValueError) as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+    # Apply overlay
+    modified_doc = apply_overlay(openapi_doc, overlay)
+
+    if args.output:
+        # Save the result to the specified file
+        save_file(modified_doc, args.output)
+        print(f'Modified OpenAPI document saved to {args.output}')
+    else:
+        # Output the result to the console
+        if args.openapi.endswith(('.yaml', '.yml')):
+            yaml.Dumper.ignore_aliases = lambda *args : True
+            print(yaml.dump(modified_doc, sort_keys=False, default_flow_style=False))
+        elif args.openapi.endswith('.json'):
+            print(json.dumps(modified_doc, indent=2))
