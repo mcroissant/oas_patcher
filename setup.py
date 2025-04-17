a1364e8ef90f2729af0a364213a7d10ba43e3d7b
Matthieu Croissant <matthieu.croissant@gmail.com>
2025-04-17 13:12:55 +0200

    rm build dir and pychache,  add to gitignore

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ print(find_packages(where='src'))  # Debugging: Print detected packages
 
 setup(
     name='oas_patch',
-    version='0.1.0',
+    version='0.1.1',
     description='A tool to apply overlays to OpenAPI documents.',
     author='Matthieu Croissant',
     url='https://github.com/mcroissant/oas_patcher',
