ed58738aa59b705f371074800bebf8c8c318039b
Matthieu Croissant <matthieu.croissant@gmail.com>
2025-09-22 17:28:45 +0200

    Bump version and update description

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='oas_patch',
-    version='1.0.0',
-    description='A tool to apply overlays to OpenAPI documents, and create an overlay from the difference of 2 openapi files.',
+    version='0.5.6',
+    description='A complete cli/lib to apply overlays to OpenAPI documents, powering your ci/cd and API lifecycle',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Matthieu Croissant',
